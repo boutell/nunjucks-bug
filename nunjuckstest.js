549c1295d3db15f1ad549e21fe1b54c75c665e3c
Tom Boutell <tom@punkave.com>
2013-09-26 09:57:08 -0400

    Bug?

---
+++ nunjuckstest.js
@@ -0,0 +1,6 @@
+var nunjucks = require('nunjucks');
+
+var env = new nunjucks.Environment(new nunjucks.FileSystemLoader(__dirname + '/views'));
+var indexTmpl = env.getTemplate('index.html');
+var reportTmpl = env.getTemplate('report.html');
+console.log(reportTmpl.render({}));
